aece3b53a1d7592422271541f5dbbeb96ed87aef
Brandon Lee <brandon@logit.global>
2019-06-25 10:50:24 +0800

    add quality on iOS

--- src/ios/CDVCapture.m
+++ src/ios/CDVCapture.m
@@ -224,6 +224,7 @@
     // taking more than one video (limit) is only supported if provide own controls via cameraOverlayView property
     NSNumber* duration = [options objectForKey:@"duration"];
     NSString* mediaType = nil;
+    NSNumber* quality = [options objectForKey:@"quality"];
 
     if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
         // there is a camera, it is available, make sure it can do movies
@@ -266,6 +267,13 @@
         // iOS 4.0
         if ([pickerController respondsToSelector:@selector(cameraCaptureMode)]) {
             pickerController.cameraCaptureMode = UIImagePickerControllerCameraCaptureModeVideo;
+            
+            if ([quality intValue] == 0) {
+                pickerController.videoQuality = UIImagePickerControllerQualityTypeLow;
+            } else {
+                pickerController.videoQuality = UIImagePickerControllerQualityTypeHigh;
+            }
+
             // pickerController.videoQuality = UIImagePickerControllerQualityTypeHigh;
             // pickerController.cameraDevice = UIImagePickerControllerCameraDeviceRear;
             // pickerController.cameraFlashMode = UIImagePickerControllerCameraFlashModeAuto;
